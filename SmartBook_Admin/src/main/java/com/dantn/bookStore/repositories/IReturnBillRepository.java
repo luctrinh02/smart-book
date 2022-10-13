b785c3028b920b36f04286f73937bc3c4c1a41a3
DESKTOP-TO7ENMK\NTS <luctt@seatechit.com.vn>
2022-10-13 12:29:52 +0700

    .

--- SmartBook_Admin/src/main/java/com/dantn/bookStore/repositories/IReturnBillRepository.java
+++ SmartBook_Admin/src/main/java/com/dantn/bookStore/repositories/IReturnBillRepository.java
@@ -1,11 +1,13 @@
 package com.dantn.bookStore.repositories;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dantn.bookStore.entities.ReturnBill;
 import com.dantn.bookStore.entities.BillStatus;
-
+@Repository
 public interface IReturnBillRepository extends JpaRepository<ReturnBill, Integer>{
-    Page<ReturnBill> findByStatus(BillStatus status);
+    Page<ReturnBill> findByStatus(BillStatus status,Pageable pageable);
 }
--- SmartBook_Admin/src/main/java/com/dantn/bookStore/services/ReturnBillService.java
+++ SmartBook_Admin/src/main/java/com/dantn/bookStore/services/ReturnBillService.java
@@ -1,8 +1,36 @@
 package com.dantn.bookStore.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.dantn.bookStore.dto.request.BillUpdateRequest;
+import com.dantn.bookStore.entities.BillStatus;
+import com.dantn.bookStore.entities.ReturnBill;
+import com.dantn.bookStore.repositories.IReturnBillRepository;
+import com.dantn.bookStore.ultilities.AppConstraint;
+import com.dantn.bookStore.ultilities.BillStatusSingleton;
 
 @Service
 public class ReturnBillService {
+    private IReturnBillRepository repository;
+
+    public ReturnBillService(IReturnBillRepository repository) {
+        super();
+        this.repository = repository;
+    }
     
+    public ReturnBill save(ReturnBill returnBill) {
+        return this.repository.save(returnBill);
+    }
+    
+    public Page<ReturnBill> getReturnBills(Integer page,BillStatusService billStatusService,Integer statusIndex){
+        BillStatus status=BillStatusSingleton.getInstance(billStatusService).get(statusIndex);
+        return this.repository.findByStatus(status,PageRequest.of(0, AppConstraint.PAGE_NUM,Sort.by("id").ascending()));
+    }
+    
+    public ReturnBill changeStatus(BillUpdateRequest request) {
+        return null;
+    }
 }
