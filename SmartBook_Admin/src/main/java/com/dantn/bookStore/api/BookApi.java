536f88e6460efd819843c59d0d287887dae30069
phuthai02 <doanphuthai1152002@gmail.com>
2022-10-31 16:02:39 +0700

    thaidp book admin

--- SmartBook_Admin/src/main/java/com/dantn/bookStore/api/BookApi.java
+++ SmartBook_Admin/src/main/java/com/dantn/bookStore/api/BookApi.java
@@ -3,11 +3,13 @@ package com.dantn.bookStore.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dantn.bookStore.dto.request.BookParamsRequest;
+import com.dantn.bookStore.entities.Book;
 import com.dantn.bookStore.services.BookService;
 
 
@@ -25,6 +27,29 @@ public class BookApi {
 	public ResponseEntity<?> getBooks(@RequestBody BookParamsRequest params) {
 		return ResponseEntity.ok(bookService.getBooks(params));
 	}
+	
+	
+	@PutMapping("/update")
+	public ResponseEntity<?> update(@RequestBody Book book) {
+		return ResponseEntity.ok(bookService.update(book));
+	}
+	
+	@PostMapping("/createType")
+	public ResponseEntity<?> createType(@RequestBody String newType) {
+		return ResponseEntity.ok(bookService.createType(newType));
+	}
+	
+	@PostMapping("/createContent")
+	public ResponseEntity<?> createContent(@RequestBody String newContent) {
+		return ResponseEntity.ok(bookService.createContent(newContent));
+	}
+
+	@PostMapping("/createCharactor")
+	public ResponseEntity<?> createCharactor(@RequestBody String newCharactor) {
+		return ResponseEntity.ok(bookService.createCharactor(newCharactor));
+	}
+	
+	
 	
 	
 	
