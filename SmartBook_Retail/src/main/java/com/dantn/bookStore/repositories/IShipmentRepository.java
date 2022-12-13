2d58eeb2e3b9d7ecded58c8738d3f85853441a85
DESKTOP-TO7ENMK\NTS <luctt@seatechit.com.vn>
2022-12-13 14:15:38 +0700

    luctt change convert money

--- SmartBook_Retail/src/main/java/com/dantn/bookStore/repositories/IShipmentRepository.java
+++ SmartBook_Retail/src/main/java/com/dantn/bookStore/repositories/IShipmentRepository.java
@@ -1,12 +1,12 @@
 package com.dantn.bookStore.repositories;
 
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dantn.bookStore.entities.Shipment;
 @Repository
 public interface IShipmentRepository extends JpaRepository<Shipment, Integer>{
-	@Query("SELECT s FROM Shipment s WHERE s.billId=?1 s.bill=?2")
-	Shipment findByBillId(Integer id,boolean isBill);
+	@Query("SELECT s FROM Shipment s WHERE s.billId=?1 and s.bill=?2")
+	Shipment getByBillId(Integer billId,Boolean bill);
 }
